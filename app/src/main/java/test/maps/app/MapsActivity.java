74b202b2a5140b89a12fd6621e72f3f1533978e5
Ankur Gaur <nkrgaur26@gmail.com>
2015-07-12 10:57:54 +0530

    Working demo with current location marker with Google Maps V2.

--- app/src/main/java/test/maps/app/MapsActivity.java
+++ app/src/main/java/test/maps/app/MapsActivity.java
@@ -1,284 +1,240 @@
 package test.maps.app;
 
-import android.content.IntentSender;
-import android.graphics.Point;
-import android.location.Location;
+import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.SystemClock;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.animation.Interpolator;
-import android.view.animation.LinearInterpolator;
-import android.widget.ScrollView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.airbnb.android.airmapview.AirMapInterface;
-import com.airbnb.android.airmapview.AirMapMarker;
-import com.airbnb.android.airmapview.AirMapView;
-import com.airbnb.android.airmapview.AirMapViewTypes;
-import com.airbnb.android.airmapview.DefaultAirMapViewBuilder;
-import com.airbnb.android.airmapview.listeners.OnCameraChangeListener;
-import com.airbnb.android.airmapview.listeners.OnCameraMoveListener;
-import com.airbnb.android.airmapview.listeners.OnLatLngScreenLocationCallback;
-import com.airbnb.android.airmapview.listeners.OnInfoWindowClickListener;
-import com.airbnb.android.airmapview.listeners.OnMapClickListener;
-import com.airbnb.android.airmapview.listeners.OnMapInitializedListener;
-import com.airbnb.android.airmapview.listeners.OnMapMarkerClickListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+import android.location.Location;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.Projection;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.Random;
+public class MapsActivity extends Activity implements ConnectionCallbacks,
+        OnConnectionFailedListener, LocationListener {
 
+    // LogCat tag
+    private static final String TAG = MapsActivity.class.getSimpleName();
 
-public class MapsActivity extends ActionBarActivity
-        implements OnCameraChangeListener, OnMapInitializedListener,
-        OnMapClickListener, OnCameraMoveListener, OnMapMarkerClickListener,
-        OnInfoWindowClickListener, OnLatLngScreenLocationCallback, GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, LocationListener {
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
 
-    private AirMapView map;
-    private DefaultAirMapViewBuilder mapViewBuilder;
-    private TextView textLogs;
-    private ScrollView logsScrollView;
+    private Location mLastLocation;
+
+    // Google client to interact with Google API
     private GoogleApiClient mGoogleApiClient;
+    private GoogleMap googleMap;
+
+    // boolean flag to toggle periodic location updates
+    private boolean mRequestingLocationUpdates = false;
+
     private LocationRequest mLocationRequest;
-    public static final String TAG = MapsActivity.class.getSimpleName();
-    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    // Location updates intervals in sec
+    private static int UPDATE_INTERVAL = 10000; // 10 sec
+    private static int FATEST_INTERVAL = 1000; // 5 sec
+    private static int DISPLACEMENT = 1; // 10 meters
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_main);
 
-        mapViewBuilder = new DefaultAirMapViewBuilder(this);
-        map = (AirMapView) findViewById(R.id.map);
-        textLogs = (TextView) findViewById(R.id.textLogs);
-        logsScrollView = (ScrollView) findViewById(R.id.logsScrollView);
+        try {
+            // Loading map
+            initilizeMap();
 
-        map.setOnMapClickListener(this);
-        map.setOnCameraChangeListener(this);
-        map.setOnCameraMoveListener(this);
-        map.setOnMarkerClickListener(this);
-        map.setOnMapInitializedListener(this);
-        map.setOnInfoWindowClickListener(this);
-        map.initialize(getSupportFragmentManager());
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-
-        // Create the LocationRequest object
-        mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
-                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
-
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
-    @Override public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
 
-    @Override public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+            // First we need to check availability of play services
+            if (checkPlayServices()) {
 
-        AirMapInterface airMapInterface = null;
+                // Building the GoogleApi client
+                buildGoogleApiClient();
 
-        if (id == R.id.action_native_map) {
-            try {
-                airMapInterface = mapViewBuilder.builder(AirMapViewTypes.NATIVE).build();
-            } catch (UnsupportedOperationException e) {
-                Toast.makeText(this, "Sorry, native Google Maps are not supported by this device. " +
-                                "Please make sure you have Google Play Services installed.",
-                        Toast.LENGTH_SHORT).show();
+                createLocationRequest();
+
+                startLocationUpdates();
+
             }
-        } else if (id == R.id.action_web_map) {
-            airMapInterface = mapViewBuilder.builder(AirMapViewTypes.WEB).build();
-        } else if (id == R.id.action_clear_logs) {
-            textLogs.setText("");
-        }
 
-        if (airMapInterface != null) {
-            map.initialize(getSupportFragmentManager(), airMapInterface);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override public void onCameraChanged(LatLng latLng, int zoom) {
-        appendLog("Map onCameraChanged triggered with lat: " + latLng.latitude + ", lng: "
-                + latLng.longitude);
-    }
-
-    @Override public void onMapInitialized() {
-        appendLog("Map onMapInitialized triggered");
-
-        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (location == null) {
-            // Blank for a moment...
-        }
-        else {
-            handleNewLocation(location);
-        };
-    }
-
-    private void addMarker(String title, LatLng latLng, int id) {
-        map.addMarker(new AirMapMarker(latLng, id)
-                .setTitle(title)
-                .setIconId(R.mipmap.icon_location_pin));
-    }
-
-    private void handleNewLocation(Location location) {
-        map.clearMarkers();
-        Random t = new Random();
-        final LatLng myLatLng = new LatLng(location.getLatitude(),location.getLongitude());
-        addMarker("You are here", myLatLng, t.nextInt(100));
-        map.animateCenterZoom(myLatLng, 30);
-    }
-
-    @Override public void onMapClick(LatLng latLng) {
-        if (latLng != null) {
-            appendLog(
-                    "Map onMapClick triggered with lat: " + latLng.latitude + ", lng: "
-                            + latLng.longitude);
-
-            map.getMapInterface().getScreenLocation(latLng, this);
-        } else {
-            appendLog("Map onMapClick triggered with null latLng");
-        }
-    }
-
-    @Override public void onCameraMove() {
-        appendLog("Map onCameraMove triggered");
-    }
-
-    private void appendLog(String msg) {
-        textLogs.setText(textLogs.getText() + "\n" + msg);
-        logsScrollView.fullScroll(View.FOCUS_DOWN);
-    }
-
-    @Override
-    public void onMapMarkerClick(long id) {
-        appendLog("Map onMapMarkerClick triggered with id " + id);
-    }
-
-    @Override public void onMapMarkerClick(Marker marker) {
-        appendLog("Map onMapMarkerClick triggered with marker " + marker.getId());
-    }
-
-    @Override public void onInfoWindowClick(long id) {
-        appendLog("Map onInfoWindowClick triggered with id " + id);
-    }
-
-    @Override public void onInfoWindowClick(Marker marker) {
-        appendLog("Map onInfoWindowClick triggered with marker " + marker.getId());
-    }
-
-    @Override public void onLatLngScreenLocationReady(Point point) {
-        appendLog("LatLng location on screen (x,y): (" + point.x + "," + point.y + ")");
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (location == null) {
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-        }
-        else {
-            handleNewLocation(location);
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.i(TAG, "Location services suspended. Please reconnect.");
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        if (connectionResult.hasResolution()) {
-            try {
-                // Start an Activity that tries to resolve the error
-                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
-            } catch (IntentSender.SendIntentException e) {
-                e.printStackTrace();
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
             }
-        } else {
-            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
         }
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        mGoogleApiClient.connect();
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        super.onPause();
+    protected void onStop() {
+        super.onStop();
         if (mGoogleApiClient.isConnected()) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
             mGoogleApiClient.disconnect();
         }
     }
 
     @Override
-    public void onLocationChanged(Location location) {
-        handleNewLocation(location);
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
     }
 
-//    public void animateMarker(final Marker marker, final LatLng toPosition,
-//                              final boolean hideMarker) {
-//        final Handler handler = new Handler();
-//        final long start = SystemClock.uptimeMillis();
-//        Projection proj =
-//        Point startPoint = proj.toScreenLocation(marker.getPosition());
-//        final LatLng startLatLng = proj.fromScreenLocation(startPoint);
-//        final long duration = 500;
-//
-//        final Interpolator interpolator = new LinearInterpolator();
-//
-//        handler.post(new Runnable() {
-//            @Override
-//            public void run() {
-//                long elapsed = SystemClock.uptimeMillis() - start;
-//                float t = interpolator.getInterpolation((float) elapsed
-//                        / duration);
-//                double lng = t * toPosition.longitude + (1 - t)
-//                        * startLatLng.longitude;
-//                double lat = t * toPosition.latitude + (1 - t)
-//                        * startLatLng.latitude;
-//                marker.setPosition(new LatLng(lat, lng));
-//
-//                if (t < 1.0) {
-//                    // Post again 16ms later.
-//                    handler.postDelayed(this, 16);
-//                } else {
-//                    if (hideMarker) {
-//                        marker.setVisible(false);
-//                    } else {
-//                        marker.setVisible(true);
-//                    }
-//                }
-//            }
-//        });
-//    }
+    /**
+     * Method to display the location on UI
+     * */
+    private void displayLocation() {
+        googleMap.clear();
+        mLastLocation = LocationServices.FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            double latitude = mLastLocation.getLatitude();
+            double longitude = mLastLocation.getLongitude();
+            MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Hello!");
+            marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+            googleMap.addMarker(marker);
+            Log.v(TAG, latitude + ", " + longitude);
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                    new LatLng(latitude, longitude)).zoom(20).build();
+
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        }
+    }
+
+
+    /**
+     * Creating google api client object
+     * */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
+
+    /**
+     * Creating location request object
+     * */
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+    }
+
+    /**
+     * Method to verify google play services on the device
+     * */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "This device is not supported.", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Starting the location updates
+     * */
+    protected void startLocationUpdates() {
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                    mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    /**
+     * Stopping location updates
+     */
+    protected void stopLocationUpdates() {
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(
+                    mGoogleApiClient, this);
+        }
+    }
+
+    /**
+     * Google api callback methods
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
+                + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+
+        // Once connected with google api, get the location
+        displayLocation();
+
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // Assign the new location
+        mLastLocation = location;
+
+        Toast.makeText(getApplicationContext(), "Location changed!",
+                Toast.LENGTH_SHORT).show();
+
+        // Displaying the new location on UI
+        displayLocation();
+    }
+
 }
